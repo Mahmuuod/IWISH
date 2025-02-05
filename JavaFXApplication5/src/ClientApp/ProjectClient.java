93483991da717090489603986f6ffbd51e833925
Randa Yasser <randa.yasser1999@gmail.com>
2025-02-05 16:10:51 +0200

    Added show friendlist

--- JavaFXApplication5/src/ClientApp/ProjectClient.java
+++ JavaFXApplication5/src/ClientApp/ProjectClient.java
@@ -19,7 +19,7 @@ public class ProjectClient extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FriendList.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Test.fxml"));
         
         Scene scene = new Scene(root);
         
