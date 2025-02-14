c06ead313c30cedf390ddba166e9404675fc6130
Mahmoud Osama <mahmoud@osama.com>
2025-02-14 20:28:10 +0200

    highlight

--- JavaFXApplication5/src/ClientApp/WishListController.java
+++ JavaFXApplication5/src/ClientApp/WishListController.java
@@ -24,16 +24,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
 public class WishListController implements Initializable {
-ServerAccess SA=new ServerAccess();
 
+    ServerAccess SA = new ServerAccess();
 
     @FXML
     private Button wishlistbtn;
@@ -68,32 +68,30 @@ ServerAccess SA=new ServerAccess();
     private Label balancelbl;
     @FXML
     private Label usernamelbl;
-  
- @FXML
- public void deleteWish (ActionEvent event) throws IOException {
-   WishInfo selectedWish = wishlisttable.getSelectionModel().getSelectedItem();
-    if (selectedWish != null) 
-    {
-        JSONObject request = new JSONObject();
-        request.put("header", "deletewish");
-        request.put("wishid",selectedWish.getWish_id());
-        request.put("userid", UserInfo.getUser().getUser_id());
-        System.out.println(selectedWish.getWish_id());
-          SA.ServerInit();
-          SA.ServerWrite(request);
-          SA.ServerRead();
-        Utilities.ChangeScene("WishList.fxml", event);
-        SA.ServerKill();
+
+    @FXML
+    public void deleteWish(ActionEvent event) throws IOException {
+        WishInfo selectedWish = wishlisttable.getSelectionModel().getSelectedItem();
+        if (selectedWish != null) {
+            JSONObject request = new JSONObject();
+            request.put("header", "deletewish");
+            request.put("wishid", selectedWish.getWish_id());
+            request.put("userid", UserInfo.getUser().getUser_id());
+            System.out.println(selectedWish.getWish_id());
+            SA.ServerInit();
+            SA.ServerWrite(request);
+            SA.ServerRead();
+            Utilities.ChangeScene("WishList.fxml", event);
+            SA.ServerKill();
+        }
+
+        //  request.put("Wish_id", selectedWish.getWishId());
+        // JSONObject response = ClientSocket.sendRequest(request);
+        // System.out.println("Server Response: " + response.getString("message"));
     }
-    
-  //  request.put("Wish_id", selectedWish.getWishId());
-
-   // JSONObject response = ClientSocket.sendRequest(request);
-   // System.out.println("Server Response: " + response.getString("message"));
-}
 
 
- /*private void switchToFriendRequestScene(ActionEvent event, UserInfo loggedInUser) {
+    /*private void switchToFriendRequestScene(ActionEvent event, UserInfo loggedInUser) {
     try {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Friendrequest.fxml"));
         Parent root = loader.load();
@@ -110,102 +108,118 @@ ServerAccess SA=new ServerAccess();
     }
 }*/
     @Override
-public void initialize(URL url, ResourceBundle rb) {
-       usernamelbl.setText(UserInfo.getUser().getUsername());
-       balancelbl.setText(String.valueOf(UserInfo.getUser().getUser_balance()));
+    public void initialize(URL url, ResourceBundle rb) {
+        usernamelbl.setText(UserInfo.getUser().getUsername());
+        balancelbl.setText(String.valueOf(UserInfo.getUser().getUser_balance()));
 
-ObservableList<WishInfo> wishesTable=FXCollections.observableArrayList();
-ItemName.setCellValueFactory(new PropertyValueFactory<>("Item_Name"));
-PriceCol.setCellValueFactory(new PropertyValueFactory<>("Item_Price"));
-wishdateCol.setCellValueFactory(new PropertyValueFactory<>("WISH_DATE"));
-LeftCol.setCellValueFactory(new PropertyValueFactory<>("contribution_amount"));
+        ObservableList<WishInfo> wishesTable = FXCollections.observableArrayList();
+        ItemName.setCellValueFactory(new PropertyValueFactory<>("Item_Name"));
+        PriceCol.setCellValueFactory(new PropertyValueFactory<>("Item_Price"));
+        wishdateCol.setCellValueFactory(new PropertyValueFactory<>("WISH_DATE"));
+        LeftCol.setCellValueFactory(new PropertyValueFactory<>("contribution_amount"));
 //WishId.setCellValueFactory(new PropertyValueFactory<>("wish_id") );
-        
-        
-        int user_id=UserInfo.getUser().getUser_id();
-        JSONObject request=new JSONObject();
+
+        wishlisttable.setRowFactory(tv -> new TableRow<WishInfo>() {
+            @Override
+            protected void updateItem(WishInfo wish, boolean empty) {
+                super.updateItem(wish, empty);
+                if (empty || wish == null) {
+                    setStyle(""); // Reset style for empty rows
+                }
+                else
+                { 
+                    if (wish.getContribution_amount() == wish.getItem_Price()) {
+                        setStyle("-fx-background-color: #98FF98;");
+
+                    }
+                    else
+                    {                    setStyle(""); // Reset style for empty rows
+}
+                }
+            }
+        });
+
+        int user_id = UserInfo.getUser().getUser_id();
+        JSONObject request = new JSONObject();
         request.put("header", "wishlist");
         request.put("user_id", UserInfo.getUser().getUser_id());
         SA.ServerInit();
         SA.ServerWrite(request);
         System.out.println(request);
 
-        JSONObject wishes=SA.ServerRead();
+        JSONObject wishes = SA.ServerRead();
         System.out.println(wishes);
 
-    
-   
-   if(wishes.getString("header").equals("wishlist"))
-   {
-       JSONArray wishesArray = new JSONArray(wishes.getJSONArray("wishes"));
-       // wishesTable
-        for (int i = 0; i < wishesArray.length(); i++) {
-            JSONObject wish = wishesArray.getJSONObject(i);
-            System.out.println(wish);
-            String Item_Name = wish.getString("Item_Name");
-            String WISH_DATE = wish.getString("WISH_DATE");
-            int Item_Price = wish.getInt("Item_Price");
-            int contribution_amount = wish.getInt("contribution_amount");
-            int wish_id=wish.getInt("wish_id");
-             info=new WishInfo(Item_Name,Item_Price,Date.valueOf(WISH_DATE),contribution_amount,wish_id);
-            System.out.println(info.toString());
-            wishesTable.add(info);
-           
-        }
+        if (wishes.getString("header").equals("wishlist")) {
+            JSONArray wishesArray = new JSONArray(wishes.getJSONArray("wishes"));
+            // wishesTable
+            for (int i = 0; i < wishesArray.length(); i++) {
+                JSONObject wish = wishesArray.getJSONObject(i);
+                System.out.println(wish);
+                String Item_Name = wish.getString("Item_Name");
+                String WISH_DATE = wish.getString("WISH_DATE");
+                int Item_Price = wish.getInt("Item_Price");
+                int contribution_amount = wish.getInt("contribution_amount");
+                int wish_id = wish.getInt("wish_id");
+                info = new WishInfo(Item_Name, Item_Price, Date.valueOf(WISH_DATE), contribution_amount, wish_id);
+                System.out.println(info.toString());
+                wishesTable.add(info);
 
-       
-       
-        wishlisttable.setItems(wishesTable);
-           SA.ServerKill(); 
-   }
-    }    
+            }
+
+            wishlisttable.setItems(wishesTable);
+            SA.ServerKill();
+        }
+    }
     WishInfo info;
-    
-/* public void setUserData(UserInfo user) {
+
+    /* public void setUserData(UserInfo user) {
     this.userData = user;
     System.out.println("Requester ID (Logged-in user): " + userData.getUser_id());
    
 }*/
-
     private void contributionlistfn(ActionEvent event) {
-                    Utilities.ChangeScene("Friendrequest.fxml",event);
+        Utilities.ChangeScene("Friendrequest.fxml", event);
 
     }
 
     @FXML
     private void addbalancefn(ActionEvent event) {
-        Utilities.ChangeScene("Addbalance.fxml",event);
+        Utilities.ChangeScene("Addbalance.fxml", event);
 
     }
 
     @FXML
     private void notificationfn(ActionEvent event) {
-       Utilities.ChangeScene("Notifications.fxml",event);
+        Utilities.ChangeScene("Notifications.fxml", event);
 
     }
 
     @FXML
     private void logoutfn(ActionEvent event) {
-      Utilities.ChangeScene("SignIn.fxml",event);
+        Utilities.ChangeScene("SignIn.fxml", event);
 
     }
-            @FXML
+
+    @FXML
     private void friendrequestbtn(ActionEvent event) {
 
-                Utilities.ChangeScene("Friendrequest.fxml",event);
+        Utilities.ChangeScene("Friendrequest.fxml", event);
     }
 
     @FXML
-   public void refreshWish(ActionEvent event) throws IOException {
-       Utilities.ChangeScene("WishList.fxml", event);
-   }
-    @FXML
-   public void friendlistbtn(ActionEvent event) throws IOException {
-       Utilities.ChangeScene("FriendList.fxml", event);
-   }
-    @FXML
-   public void itemsBtn (ActionEvent event) throws IOException {
-       Utilities.ChangeScene("Item.fxml", event);
+    public void refreshWish(ActionEvent event) throws IOException {
+        Utilities.ChangeScene("WishList.fxml", event);
+    }
 
-   }
+    @FXML
+    public void friendlistbtn(ActionEvent event) throws IOException {
+        Utilities.ChangeScene("FriendList.fxml", event);
+    }
+
+    @FXML
+    public void itemsBtn(ActionEvent event) throws IOException {
+        Utilities.ChangeScene("Item.fxml", event);
+
+    }
 }
